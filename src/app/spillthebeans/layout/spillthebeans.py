d39cb97e83dd81d0d88ae0d8b63ea309e5a967b5
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2022-10-12 14:14:51 +0100

    Updated requirements and added github link

--- src/app/spillthebeans/layout/spillthebeans.py
+++ src/app/spillthebeans/layout/spillthebeans.py
@@ -69,7 +69,30 @@ class SpillthebeansAIO(html.Div):
                         "display":"block",
                         "bottom": "0px",
                         "margin": "20px",
-                        # "background-color":"transparent"
+                    }
+                ),
+                html.Div(
+                    dbc.Button(
+                        html.Img(
+                            src="/assets/GitHub-Mark-32px.png",
+                            style={
+                                "height":"32px",
+                                "width":"32px"
+                            }
+                        ),
+                        href="https://github.com/thisistheplace",
+                        external_link=True,
+                        outline=True,
+                        size="lg",
+                        n_clicks=0,
+                    ),
+                    style={
+                        "zIndex": "10",
+                        "position":"absolute",
+                        "display":"block",
+                        "top": "0px",
+                        "right": "0px",
+                        "margin": "20px"
                     }
                 ),
                 dbc.Offcanvas(
