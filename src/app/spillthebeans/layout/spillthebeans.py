3e9833b2de7d135c23d9a98531bc6d22d48f7d04
Ben Cannell <bencannell@bens-air.home>
2023-01-06 10:43:31 +0000

    Added page loader

--- src/app/spillthebeans/layout/spillthebeans.py
+++ src/app/spillthebeans/layout/spillthebeans.py
@@ -1,4 +1,5 @@
 from dash import Output, Input, html, callback, MATCH
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import math
 import uuid
@@ -34,137 +35,143 @@ class SpillthebeansAIO(html.Div):
         # Define the component's layout
         super().__init__(
             [  # Equivalent to `html.Div([...])`
-                html.Div(
-                    SpillthebeansThreejs(
-                        id=self.ids.three(aio_id),
-                        canAngle=math.pi * -0.75,
-                        scale=0.075
-                    ),
-                    style={
-                        "zIndex": "5",
-                        "position":"absolute",
-                        "display":"block",
-                        "height": "100%",
-                        "width": "100%"
-                    }
-                ),
-                html.Div(
-                    dbc.Button(
+                dcc.Loading(
+                    id="page-loading",
+                    type="circle",
+                    children=[
                         html.Div(
-                            html.I(
-                                className="fa-solid fa-chevron-up"
+                            SpillthebeansThreejs(
+                                id=self.ids.three(aio_id),
+                                canAngle=math.pi * -0.75,
+                                scale=0.075
                             ),
-                            className="animate__flash"
-                        ),
-                        id=self.ids.sidebar_open(aio_id),
-                        className="me-1",
-                        outline=True,
-                        size="lg",
-                        n_clicks=0,
-                    ),
-                    style={
-                        "zIndex": "10",
-                        "position":"absolute",
-                        "display":"block",
-                        "bottom": "0px",
-                        "margin": "20px",
-                    }
-                ),
-                dbc.Tooltip(
-                    "portfolio",
-                    id="tooltip",
-                    is_open=True,
-                    target=self.ids.sidebar_open(aio_id),
-                    placement="top"
-                ),
-                html.Div(
-                    dbc.Button(
-                        html.Img(
-                            src="/assets/GitHub-Mark-32px.png",
                             style={
-                                "height":"32px",
-                                "width":"32px"
+                                "zIndex": "5",
+                                "position":"absolute",
+                                "display":"block",
+                                "height": "100%",
+                                "width": "100%"
                             }
                         ),
-                        href="https://github.com/thisistheplace",
-                        external_link=True,
-                        outline=True,
-                        size="lg",
-                        n_clicks=0,
-                    ),
-                    style={
-                        "zIndex": "10",
-                        "position":"absolute",
-                        "display":"block",
-                        "top": "0px",
-                        "right": "0px",
-                        "margin": "20px"
-                    }
-                ),
-                html.Div(
-                    dbc.Button(
-                        html.I(
-                            className="fa-solid fa-envelope"
+                        html.Div(
+                            dbc.Button(
+                                html.Div(
+                                    html.I(
+                                        className="fa-solid fa-chevron-up"
+                                    ),
+                                    className="animate__flash"
+                                ),
+                                id=self.ids.sidebar_open(aio_id),
+                                className="me-1",
+                                outline=True,
+                                size="lg",
+                                n_clicks=0,
+                            ),
+                            style={
+                                "zIndex": "10",
+                                "position":"absolute",
+                                "display":"block",
+                                "bottom": "0px",
+                                "margin": "20px",
+                            }
                         ),
-                        href="mailto:bean.can.design@gmail.com",
-                        external_link=True,
-                        outline=True,
-                        size="lg",
-                        n_clicks=0,
-                    ),
-                    style={
-                        "zIndex": "10",
-                        "position":"absolute",
-                        "display":"block",
-                        "top": "0px",
-                        "left": "0px",
-                        "margin": "20px"
-                    }
-                ),
-                dbc.Offcanvas(
-                    dbc.Container(
-                        dbc.Row(
-                            dbc.Col(
-                                dbc.Row(
-                                    [
-                                        dbc.Col(
-                                            CardAIO(card=card),
-                                            width=12,
-                                            sm=12,
-                                            md=6,
-                                            lg=4
-                                        )
-                                        for card in cards
-                                    ],
-                                    justify="left",
-                                    align="left",
+                        dbc.Tooltip(
+                            "portfolio",
+                            id="tooltip",
+                            is_open=True,
+                            target=self.ids.sidebar_open(aio_id),
+                            placement="top"
+                        ),
+                        html.Div(
+                            dbc.Button(
+                                html.Img(
+                                    src="/assets/GitHub-Mark-32px.png",
                                     style={
-                                        "width":"100%",
-                                        "flexFlow": "row",
-                                        "overflowX": "scroll"
+                                        "height":"32px",
+                                        "width":"32px"
                                     }
                                 ),
-                                width=12,
-                                sm=12,
-                                md=10,
-                                lg=12
+                                href="https://github.com/thisistheplace",
+                                external_link=True,
+                                outline=True,
+                                size="lg",
+                                n_clicks=0,
                             ),
-                            justify="center"
-                        )
-                    ),
-                    id=self.ids.card_row(aio_id),
-                    placement="bottom",
-                    is_open=False,
-                    backdrop=True,
-                    close_button=False,
-                    style={
-                        "backgroundColor":"transparent",
-                        "borderStyle":"none",
-                        "height":"fit-content",
-                        "maxHeight": "80vh",
-                        "paddingBottom": "20px"
-                    }
-                ),
+                            style={
+                                "zIndex": "10",
+                                "position":"absolute",
+                                "display":"block",
+                                "top": "0px",
+                                "right": "0px",
+                                "margin": "20px"
+                            }
+                        ),
+                        html.Div(
+                            dbc.Button(
+                                html.I(
+                                    className="fa-solid fa-envelope"
+                                ),
+                                href="mailto:bean.can.design@gmail.com",
+                                external_link=True,
+                                outline=True,
+                                size="lg",
+                                n_clicks=0,
+                            ),
+                            style={
+                                "zIndex": "10",
+                                "position":"absolute",
+                                "display":"block",
+                                "top": "0px",
+                                "left": "0px",
+                                "margin": "20px"
+                            }
+                        ),
+                        dbc.Offcanvas(
+                            dbc.Container(
+                                dbc.Row(
+                                    dbc.Col(
+                                        dbc.Row(
+                                            [
+                                                dbc.Col(
+                                                    CardAIO(card=card),
+                                                    width=12,
+                                                    sm=12,
+                                                    md=6,
+                                                    lg=4
+                                                )
+                                                for card in cards
+                                            ],
+                                            justify="left",
+                                            align="left",
+                                            style={
+                                                "width":"100%",
+                                                "flexFlow": "row",
+                                                "overflowX": "scroll"
+                                            }
+                                        ),
+                                        width=12,
+                                        sm=12,
+                                        md=10,
+                                        lg=12
+                                    ),
+                                    justify="center"
+                                )
+                            ),
+                            id=self.ids.card_row(aio_id),
+                            placement="bottom",
+                            is_open=False,
+                            backdrop=True,
+                            close_button=False,
+                            style={
+                                "backgroundColor":"transparent",
+                                "borderStyle":"none",
+                                "height":"fit-content",
+                                "maxHeight": "80vh",
+                                "paddingBottom": "20px"
+                            }
+                        ),
+                    ]
+                )
             ],
             style={"height": "100vh", "width": "100vw"},
         )
